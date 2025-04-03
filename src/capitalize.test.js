5b6739ffffb94b2bd062029aa9527f2d69eb24e3
Edmond Chu <edmondchu10@hotmail.com>
2025-04-03 17:17:21 +0100

    test: add capitalize test

--- src/capitalize.test.js
+++ src/capitalize.test.js
@@ -2,15 +2,19 @@ import { capitalize } from "./capitalize.js";
 
 describe("capitalize", () => {
     test("should capitalize a lower case word", () => {
-        expect(capitalize('hello')).toMatch('Hello');
+        expect(capitalize("hello")).toMatch("Hello");
     });
 
     test("should capitalize a lower case sentence", () => {
-        expect(capitalize('good morning to you all.')).toMatch('Good morning to you all.');
+        expect(capitalize("good morning to you all.")).toMatch("Good morning to you all.");
     });
 
     test("should not change when word is already capitalized", () => {
-        expect(capitalize('Howdy')).toMatch('Howdy');
+        expect(capitalize("Howdy")).toMatch("Howdy");
     });
+
+    test("should not change when number at start", () => {
+        expect(capitalize("5 words")).toMatch("5 words");
+    })
 })
 
